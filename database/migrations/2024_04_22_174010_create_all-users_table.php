9cbf1b0f6a441c3cbee73b027c0c29529295249f
Ahmed-hagag8 <157177560+Ahmed-hagag8@users.noreply.github.com>
2024-04-22 19:41:39 +0200

    update1

---
+++ database/migrations/2024_04_22_174010_create_all-users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAllUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data-base-project', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data-base-project');
+    }
+}
